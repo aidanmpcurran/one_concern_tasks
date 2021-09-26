1cd72a34a4e8f32f7db4089a6eddfbb40ad686df
aidancurran42702 <aidan.miguel.curran@berkeley.edu>
2021-09-26 15:26:06 -0700

    part2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const Header = (props) => {
+  return
+
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Contenttext = [
+    {part1: 'Fundamentals of React', exercises1:10},
+    {part2: 'Using props to pass data',exercises2: 7},
+    {part3: 'State of a component', exercises3: 14}
+  ]
+
+  const Totals = {exercises1, exercises2, exercises3}
+
+  const Content = (TextforContents) => {
+    return (
+        <div>
+          <p>{TextforContents}</p>
+
+        </div>
+
+    )
+  }
+
+  const Total = () =>{
+    exercises1
+    exercises2
+    exercises3
+  }
+
+  const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course = {course}/>
+      <Content partArray = {Contenttext}/>
+      <Total ... />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
